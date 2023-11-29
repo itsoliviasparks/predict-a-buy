1c53f923cf99202033552fe8d159858b42248bd7
itsoliviasparks <itsoliviasparks@gmail.com>
2023-11-28 18:19:04 -0700

    migrating to personal firestore database

--- src/api/config.js
+++ src/api/config.js
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcBMAQcA2VkfOFUS6toxpx4zbFLJOKgwA",
-  authDomain: "tcl-61-smart-shopping-list.firebaseapp.com",
-  projectId: "tcl-61-smart-shopping-list",
-  storageBucket: "tcl-61-smart-shopping-list.appspot.com",
-  messagingSenderId: "103429211108",
-  appId: "1:103429211108:web:2e5a2dc705c12c415efdf1"
+	apiKey: 'AIzaSyDp4TZd4myXzzsG8OIZe4JoXll8odAfJ4k',
+	authDomain: 'predict-a-buy.firebaseapp.com',
+	databaseURL: 'https://predict-a-buy-default-rtdb.firebaseio.com',
+	projectId: 'predict-a-buy',
+	storageBucket: 'predict-a-buy.appspot.com',
+	messagingSenderId: '139758585971',
+	appId: '1:139758585971:web:adc285b59988f34ac58206',
 };
 
 // Initialize Firebase
